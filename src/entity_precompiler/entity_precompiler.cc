889de433867384972bb88eae548f514ddce6249e
George Gerdin <georgegerdin@gmail.com>
2023-08-13 14:31:13 +0200

    Testing using cpp-parser from entity_precompiler.

--- src/entity_precompiler/entity_precompiler.cc
+++ src/entity_precompiler/entity_precompiler.cc
@@ -3,6 +3,11 @@
 auto c_str = "int main(int, char**) {return 0;}";
 
 int main(int argc, char* argv[]) {
+    std::vector<std::string> arguments;
+    for(std::size_t i = 0; i < argc; ++i) {
+        arguments.push_back(argv[i]);
+    }
+
     Cpp::Preprocessor preprocessor("main.cc", c_str);
     auto tokens = preprocessor.process_and_lex();
     Cpp::Parser parser(tokens, "main.cc");
