45d2b6b437f7beaf3a17e5498fa2fb9694d66c32
George Gerdin <georgegerdin@gmail.com>
2023-12-03 13:14:09 +0100

    Make the precompiler list namespaces and structs in the file

--- src/entity_precompiler/entity_precompiler.cc
+++ src/entity_precompiler/entity_precompiler.cc
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include "cpp_parser/parser.hh"
-
-auto c_str = "int main(int, char**) {return 0;}";
+#include "cpp_parser/util.hh"
+#include "cpp_parser/traverse_ast.hh"
 
 int main(int argc, char* argv[]) {
     std::vector<std::string> arguments;
@@ -21,9 +21,29 @@ int main(int argc, char* argv[]) {
 
     outln("arguments: {}\ninput: {}\noutput: {}", arguments.size(), input_file_path, output_file_path);
 
-    Cpp::Preprocessor preprocessor(input_file_path, c_str);
+    std::ifstream input_file(input_file_path);
+    if(!input_file.is_open()) {
+        outln("Unable to open {}", input_file_path);
+        return -1;
+    }
+
+    // Load the text
+    std::string input_file_contents((std::istreambuf_iterator<char>(input_file)), std::istreambuf_iterator<char>());
+    input_file.close();
+
+    Cpp::Preprocessor preprocessor(input_file_path, input_file_contents);
     auto tokens = preprocessor.process_and_lex();
     Cpp::Parser parser(tokens, input_file_path);
+
+    auto translation_unit = parser.parse();
+    traverse_ast_tree(translation_unit, [] (Cpp::AstNodePtr node_ptr) {
+        std::visit(Cpp::overloaded{
+                [](Cpp::StructOrClassDeclaration const* soc) { outln("StructOrClass {}", soc->full_name()); },
+                [](Cpp::NamespaceDeclaration const* nd) { outln("Namespace {}", nd->full_name()); },
+                [](auto const* node) {
+                }
+        }, node_ptr);
+    });
 
     std::ofstream output(output_file_path);
     if(output.is_open()) {
--- src/game/entities.hh
+++ src/game/entities.hh
@@ -5,4 +5,14 @@
 #ifndef ROGUELIKE_ENTITIES_HH
 #define ROGUELIKE_ENTITIES_HH
 
+namespace Entities {
+    struct Player {
+
+    };
+
+    struct Flower {
+
+    };
+}
+
 #endif //ROGUELIKE_ENTITIES_HH
