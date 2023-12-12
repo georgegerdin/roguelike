8cde97110a4ac71ed4ad80be93617c474eae2ebf
George Gerdin <georgegerdin@gmail.com>
2023-12-12 17:38:22 +0100

    Add a main loop displaying an imgui window

--- src/game/game.cc
+++ src/game/game.cc
@@ -2,6 +2,7 @@
 // Created by george on 2023-12-03.
 //
 #include <cstdio>
+#include <imgui/imgui.h>
 #include "entities.hh"
 
 const char* entity_type(Entities::Entity const& e) {
@@ -13,6 +14,11 @@ const char* entity_type(Entities::Entity const& e) {
     );
 }
 
+bool imgui_init();
+void imgui_start_frame();
+bool imgui_end_frame();
+void imgui_shutdown();
+
 int main(int argc, char* argv[]) {
 
     Entities::Entity entity = Entities::Player{};
@@ -20,5 +26,20 @@ int main(int argc, char* argv[]) {
 
     Entities::Entity entity2 = Entities::Flower{};
     printf("Created another entity: %s\n", entity_type(entity2));
+
+    if (!imgui_init()) return 0;
+
+    bool done = false;
+    while(!done) {
+        imgui_start_frame();
+
+        ImGui::Begin("Image", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
+        ImGui::Text("Hello!");
+        ImGui::End();
+        done = !imgui_end_frame();
+    }
+
+
+    imgui_shutdown();
     return 0;
 }
