025fd98ff1f3c0e2fd01c4ea6df7809f81a719ec
George Gerdin <georgegerdin@gmail.com>
2023-12-16 22:26:03 +0100

    Rudimentary arrow key movement

--- src/game/game.cc
+++ src/game/game.cc
@@ -22,12 +22,12 @@ struct Position {
 
 Position entity_position(Entities::Entity const& e) {
     return std::visit(
-            [](auto const& e) -> Position {
-                Position pos;
-                e.position(&pos.x, &pos.y);
-                return pos;
-            },
-            e
+        [](auto const& e) -> Position {
+            Position pos;
+            e.position(&pos.x, &pos.y);
+            return pos;
+        },
+        e
     );
 }
 
@@ -42,10 +42,47 @@ void entity_set_position(Entities::Entity& e, Position pos) {
 
 char entity_symbol(Entities::Entity const& e) {
     return std::visit(
-            [](auto const& e) -> char {
-                return e.symbol();
-            },
-            e
+        [](auto const& e) -> char {
+            return e.symbol();
+        },
+        e
+    );
+}
+
+enum Direction {
+    Left,
+    Right,
+    Up,
+    Down,
+    UpLeft,
+    UpRight,
+    DownLeft,
+    DownRight
+};
+void entity_move(Entities::Entity& e, Direction direction) {
+    std::visit(
+        [direction](auto& e) {
+            Position pos;
+            e.position(&pos.x, &pos.y);
+
+            switch(direction) {
+                case Direction::Left:
+                    if(pos.x > 0) pos.x--;
+                    break;
+                case Direction::Right:
+                    if(pos.x < 9) pos.x++;
+                    break;
+                case Direction::Up:
+                    if(pos.y > 0) pos.y--;
+                    break;
+                case Direction::Down:
+                    if(pos.y < 9) pos.y++;
+                    break;
+            }
+
+            e.set_position(pos.x, pos.y);
+        },
+        e
     );
 }
 
@@ -54,9 +91,7 @@ class RenderFrame {
     std::array<char, WIDTH * HEIGHT> m_frame;
 public:
     RenderFrame() {
-        for(char& tile : m_frame) {
-            tile = ' ';
-        }
+        clear();
     }
 
     void set(int x, int y, char c) {
@@ -69,6 +104,12 @@ public:
 
     int width() const { return WIDTH; }
     int height() const { return HEIGHT; }
+
+    void clear() {
+        for(char& tile : m_frame) {
+            tile = ' ';
+        }
+    }
 };
 
 
@@ -108,12 +149,22 @@ int main(int argc, char* argv[]) {
     while(!done) {
         imgui_start_frame();
 
+        if(ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_LeftArrow)))
+            entity_move(entity, Direction::Left);
+        if(ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_RightArrow)))
+            entity_move(entity, Direction::Right);
+        if(ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_UpArrow)))
+            entity_move(entity, Direction::Up);
+        if(ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_DownArrow)))
+            entity_move(entity, Direction::Down);
+
         ImGui::Begin("Image", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
         ImGui::Text("Hello!");
         ImGui::PushStyleColor(ImGuiCol_ChildBg, ImVec4(0.f, 0.f, 0.f, 1.f));
         ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.f, 1.f, 1.f, 1.f));
         ImGui::BeginChild("world", ImVec2(frame_width, frame_height), true);
 
+        frame.clear();
         render_entity(entity, frame);
         render_entity(entity2, frame);
         render_frame(frame);
